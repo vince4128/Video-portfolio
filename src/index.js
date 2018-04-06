55a546fa1a29cb54864cb9107a13a2a0267076c7
vince4128 <vrlt.vincent@gmail.com>
2018-04-06 16:41:46 +0200

    rebased starter
    condition for cleaning dist added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-//styles import
+// styles import (for webpack)
 import style from "./styles/style.scss";
 
-alert("it works !");
-//coucou
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+    , document.querySelector('.app-container'));
