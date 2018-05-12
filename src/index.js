694a0fb73cdb3a2d00678fd269f562710a6f08cf
vince4128 <vrlt.vincent@gmail.com>
2018-05-12 18:18:12 +0200

    Router added

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { HashRouter, Route, Switch, Link, Redirect} from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Component
+import App from './components/App';
+import Menu from './containers/Menu';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
+      <HashRouter>
+        <App/>
+      </HashRouter>      
     </Provider>
     , document.querySelector('.app-container'));
