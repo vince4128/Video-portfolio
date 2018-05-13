297be859984708b29de1dd9e66ecc20008c8ce63
vince4128 <vrlt.vincent@gmail.com>
2018-05-13 19:00:08 +0200

    Footer added

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Menu from '../containers/Menu';
 import Main from './Main';
+import Footer from './Footer';
 
 const App = () => {
     return (<div>
         <Menu />
         <Main />
+        <Footer />
     </div>)
 }
 
