5d781d30fa8906826ee1d19eaa49e37c443216be
vince4128 <vrlt.vincent@gmail.com>
2018-05-14 14:23:25 +0200

    video component reorganised

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,11 +7,18 @@ const Video = ({video}) => {
 
     return(
         <div className="m-video">
-            <ReactPlayer url={`${video.lien}`} controls />
+            <ReactPlayer 
+                className='a-video-player' 
+                url={`${video.lien}`} 
+                controls />
             <div className="m-video-info">
-                <p><span>{video.titre}</span> <span>{video.client}</span> <span>{video.annee}</span></p>             
-                <p>{video.legende}</p>
-            </div>                        
+                <p>
+                    <span className="a-video-info-item--name">{video.titre}</span> <span className="a-video-info-item--client">{video.client}</span> - <span className="a-video-info-item--year">{video.annee}</span>&nbsp;
+                </p>             
+                <p className="a-video-info-item--desc">  {video.legende}
+                </p>
+            </div>
+            <hr/>                        
         </div>
     )
 
