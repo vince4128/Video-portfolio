0d18dcbe63a5425e375a7b5f0ff1b20c1af7e84d
vince4128 <vrlt.vincent@gmail.com>
2018-05-15 18:09:26 +0200

    responsive toggle menu (bug to fix initialisation)

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -8,15 +8,19 @@ class Menu extends Component {
 
     componentDidMount(){
         this.props.fetchCategories(()=>console.log('Catégories chargées'));
+        document.body.classList.toggle('m-menu--open', this.state.open);        
     }
 
     constructor(props){
         super(props);
 
         this.state = {
-            currentCat:0
+            currentCat:0,
+            open:false
         }
 
+        this.isOpened = this.isOpened.bind(this);
+        this.setOpen = this.setOpen.bind(this);
     }
     
     setActive(item){
@@ -24,20 +28,39 @@ class Menu extends Component {
             return "active";
         }else{
             return "";
-        }        
+        }                
+    }
 
+    isOpened(){
+        if(this.state.open){
+            return "m-menu--open";
+        } else {
+            return "";
+        }
+    }
+
+    setOpen(shouldBeToggled=true){
+        alert('shouldBeToggled est ' + shouldBeToggled + " avant tout opé open " + this.state.open);
+        if(shouldBeToggled){            
+            this.setState({ open: !this.state.open});
+            alert('shouldBeToggled ' + this.state.open);
+        }else{
+            this.setState({ open: false})
+            alert('should Be closed ' + this.state.open);
+        }
+        document.body.classList.toggle('m-menu--open', this.state.open);
     }
 
     renderList(){
 
         return _.map(this.props.categories, categorie => {
             categorie.name = categorie.titre.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/\s/g,'');
-            return(
-                <Link to={`/${categorie.name}`} className={"a-menu__item " + this.setActive(categorie.name)}>
-                <li key={categorie.id}>
-                    <p>{categorie.titre}</p>
-                </li>
-                </Link>                
+            return(                
+                <li key={categorie.id} className={"a-menu__item " + this.setActive(categorie.name)} onClick={() => this.setOpen(false)}>
+                    <Link to={`/${categorie.name}`}>
+                        {categorie.titre}
+                    </Link>  
+                </li>                              
             )
         });
         
@@ -49,21 +72,24 @@ class Menu extends Component {
             return <div>Loading</div>;
         }else{
 
-            return <div>
-            <ul className="m-menu">
-                <li className="a-menu__item"><p>Lars Blumer</p></li>
-                {this.renderList()}
-                <Link to={`/me`} className={"a-menu__item " + this.setActive('me')}>
-                    <li>
-                        <p>Me</p>
-                    </li>
-                </Link>
-                <Link to={`/contact`} className={"a-menu__item " + this.setActive('contact')}>
-                    <li>
-                        <p>Contact</p>
-                    </li>
-                </Link>
-            </ul>            
+            return <div className={"u-fixed-top u-fixed-top--menu-top "}>
+            <ul className="m-menu ">
+                <li className="a-menu__item">Lars Blumer</li>
+                {this.renderList()}                
+                <li className={"a-menu__item " + this.setActive('me')} onClick={() => this.setOpen(false)}>
+                    <Link to={`/me`}>
+                        Me
+                    </Link>
+                </li>                                
+                <li className={"a-menu__item " + this.setActive('contact')} onClick={() => this.setOpen(false)}>
+                    <Link to={`/contact`}>
+                        Contact
+                    </Link>
+                </li>  
+                <li className="a-menu__toggle" onClick={() => this.setOpen()}>
+                toggle
+                </li>              
+            </ul>          
             </div>
 
         }
