af5e50724abf5f210183839e67712039bcd577a6
vince4128 <vrlt.vincent@gmail.com>
2018-06-04 11:59:47 +0200

    me page started

--- src/components/Me.js
+++ src/components/Me.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Me = () => {
     return(
-        <div>
-            <h1>Me</h1>
-        </div>
+        <section className="m-me-hero">
+            {/*<article className="m-me-txt">
+            <p>I am a director and writer, living in Paris.</p>
+            <p>I love to eat but I am a terrible cook.</p>
+            <p>I like surfing and I am ok at it. </p>
+            <p>We will see in 50 years if I was a good father. </p>
+            <p>I have shot many short films and one feature film:</p>
+            <p>It's called Mike with Marc-André Grondin, Christa Theret and Eric Elmosnino. </p>
+            <p>I have shot many commercials. Some of them quite funny. Some won prizes. </p>
+    </article>*/}
+        </section>
     )
 }
 
