020eee96577e7dab6aa1672dc8308ad03fde69f4
vince4128 <vrlt.vincent@gmail.com>
2018-06-11 17:03:22 +0200

    text modification and Link commented

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -4,14 +4,17 @@ const Thumb = (props) => {
 
     return(
         <section>
+            {/*<Link to={`${}`}>*/}
             <article className="m-thumb animated fadeIn" onClick={() => props.onVideoSelect(props.video.id)}>
                 {/*<img src={`img/${props.img}`}></img>*/}
                 <img src={`admin/thumb/${props.img}`}></img>
             </article>
+            {/*</Link>*/}
             <div className="m-video-info">
                 <p>
-                    <span className="a-video-info-item--name">{props.video.titre}</span> • <span className="a-props.-info-item--client">{props.video.client}</span>&nbsp;
-                    <span className="a-video-info-item--desc">  {props.video.legende}</span>
+                    <span className="a-video-info-item--name">{props.video.titre}</span>&nbsp;                    
+                    <span className="a-props.-info-item--client">{props.video.client}</span>&nbsp;
+                    <span className="a-video-info-item--desc">{props.video.legende}</span>
                 </p>             
             </div>
         </section>
