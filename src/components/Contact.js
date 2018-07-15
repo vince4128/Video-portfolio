6e54245cd6ecb23fc9cf5b8db890ee9bfa8c7593
vince4128 <vrlt.vincent@gmail.com>
2018-07-15 21:11:00 +0200

    contact started

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 const Contact = () => {
     return(
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <section className="m-contact">
+
+            {/*<article className="m-contact__top">
+
+                <div>
+                    <h1 className="m-contact__name">Lars Blumers</h1>
+                    <h2 className="m-contact__mail">lars.blumers@mass.eu</h2>
+                </div>
+
+            </article>
+
+            <article className="m-contact__bottom">
+
+                <p>Commercial Representation :</p>
+
+                <div>
+                    <h1>BIG PRODUCTIONS</h1>
+                </div>
+
+                <div>                    
+
+                    <h3>19, RUE DE L'ÉCHIQUIER<br/>
+                        75010 - PARIS<br/>
+                        FRANCE
+                    </h3>
+
+                    <h3>T. (33)01 53 93 68 00<br/>
+                        F. (33)01 53 93 68 01<br/>
+                        BIG@BIGPRODUCTIONS.FR</h3>
+                </div>
+
+    </article>*/}            
+
+        </section>
     )
 }
 
