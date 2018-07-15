687bd5a355cb5d0bb6045e3e562708bcd61b1a87
vince4128 <vrlt.vincent@gmail.com>
2018-07-15 21:11:27 +0200

    me section

--- src/components/Me.js
+++ src/components/Me.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Me = () => {
     return(
-        <section className="m-me-hero">
+        <section className="m-me animated fadeIn">
             {/*<article className="m-me-txt">
             <p>I am a director and writer, living in Paris.</p>
             <p>I love to eat but I am a terrible cook.</p>
@@ -12,7 +12,23 @@ const Me = () => {
             <p>It's called Mike with Marc-André Grondin, Christa Theret and Eric Elmosnino. </p>
             <p>I have shot many commercials. Some of them quite funny. Some won prizes. </p>
     </article>*/}
-        </section>
+    
+        <div className="m-me__avatar">
+            {/*<img src="img/lars_square.jpg"/>*/}
+        </div>
+
+        <div className="m-me__text">
+            <p>I am a director and writer, living in Paris.<br/>
+            I love to eat but I am a terrible cook.<br/>
+            I like surfing and I am ok at it.<br/> 
+            We will see in 50 years if I was a good father.<br/>
+            I have shot many short films and one feature film:<br/>
+            It's called Mike with Marc-André Grondin, Christa Theret and Eric Elmosnino.<br/> 
+            I have shot many commercials. Some of them quite funny. Some won prizes. </p>
+        </div>
+
+    </section>
+    
     )
 }
 
