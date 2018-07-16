904855d47a52c4b82ef61d8a600b2f47ff636bd8
vince4128 <vrlt.vincent@gmail.com>
2018-07-16 14:40:10 +0200

    style added to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,11 +4,11 @@ const Contact = () => {
     return(
         <section className="m-contact">
 
-            {/*<article className="m-contact__top">
+            <article className="m-contact__top">
 
-                <div>
-                    <h1 className="m-contact__name">Lars Blumers</h1>
-                    <h2 className="m-contact__mail">lars.blumers@mass.eu</h2>
+                <div className="m-contact__name">
+                    <h1>Lars Blumers</h1>
+                    <h2 className="m-contact__mail"><a href="mailto:lars.blumers@mass.eu">lars.blumers@mass.eu</a></h2>
                 </div>
 
             </article>
@@ -23,17 +23,19 @@ const Contact = () => {
 
                 <div>                    
 
-                    <h3>19, RUE DE L'ÉCHIQUIER<br/>
+                    <h3><a href="https://maps.google.com/?q=19,+RUE+DE+L'%C3%89CHIQUIER+75010+-+PARIS+FRANCE&amp;entry=gmail&amp;source=g" target="_blank">19, RUE DE L'ÉCHIQUIER<br/>
                         75010 - PARIS<br/>
-                        FRANCE
-                    </h3>
+                        FRANCE</a>
+                    </h3>                    
 
                     <h3>T. (33)01 53 93 68 00<br/>
                         F. (33)01 53 93 68 01<br/>
-                        BIG@BIGPRODUCTIONS.FR</h3>
+                        <br/>                        
+                        <a href="mailto:big@bigproductions.fr">BIG@BIGPRODUCTIONS.FR</a>
+                    </h3>
                 </div>
 
-    </article>*/}            
+            </article>        
 
         </section>
     )
