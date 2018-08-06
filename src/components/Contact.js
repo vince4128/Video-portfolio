f939832669740f539404f33fd191ee41ab6ee372
varlet vincent <vrlt.vincent@gmail.com>
2018-08-06 20:28:01 +0200

    contact cards added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,27 +13,63 @@ const Contact = () => {
 
             </article>
 
+            <p className="a-contact-representation">Commercial Representation :</p>
+
             <article className="m-contact__bottom">
 
-                <p>Commercial Representation :</p>
+                <section className="m-contact__card">
+
+                    <div className="m-contact__card__flag">
+                        <p>FRANCE</p>
+                    </div>
+
+                    <div>
+                        <h1>BIG PRODUCTIONS</h1>
+                    </div>
+
+                    <div>                    
+
+                        <h3><a href="https://maps.google.com/?q=19,+RUE+DE+L'%C3%89CHIQUIER+75010+-+PARIS+FRANCE&amp;entry=gmail&amp;source=g" target="_blank">19, RUE DE L'ÉCHIQUIER<br/>
+                            75010 - PARIS<br/>
+                            FRANCE</a>
+                        </h3>                    
+
+                        <h3>T. (33)01 53 93 68 00<br/>
+                            F. (33)01 53 93 68 01<br/>
+                            <br/>                        
+                            <a href="mailto:big@bigproductions.fr">BIG@BIGPRODUCTIONS.FR</a>
+                        </h3>
+                </div>
+
+                </section>
+
+                <section className="m-contact__card">
+
+                <div className="m-contact__card__flag">
+                    <p>GERMANY</p>
+                </div>                            
 
                 <div>
-                    <h1>BIG PRODUCTIONS</h1>
+                    <h1>Gesas Management</h1>
                 </div>
 
-                <div>                    
-
-                    <h3><a href="https://maps.google.com/?q=19,+RUE+DE+L'%C3%89CHIQUIER+75010+-+PARIS+FRANCE&amp;entry=gmail&amp;source=g" target="_blank">19, RUE DE L'ÉCHIQUIER<br/>
-                        75010 - PARIS<br/>
-                        FRANCE</a>
-                    </h3>                    
-
-                    <h3>T. (33)01 53 93 68 00<br/>
-                        F. (33)01 53 93 68 01<br/>
+                <div>
+                    <h3>Gesa Müller-Reinhardt                    
+                    <br/>+49 (0) 172 45 22 609<br/>
                         <br/>                        
-                        <a href="mailto:big@bigproductions.fr">BIG@BIGPRODUCTIONS.FR</a>
+                        <a href="mailto:gesa@gesas.net">gesa@gesas.net</a>
                     </h3>
                 </div>
+
+                <div>
+                    <h3>Petra Sauer                   
+                    <br/>+49 (0) 172 45 22 609<br/>
+                        <br/>                        
+                        <a href="mailto:petra@gesas.net">petra@gesas.net</a>
+                    </h3>
+                </div>
+
+                </section>
 
             </article>        
 
