f02bfab3d6d931a875ba1add18f47ade65cc49ad
varlet vincent <vrlt.vincent@gmail.com>
2018-08-06 20:29:12 +0200

    import cleaned

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,11 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import { Link } from 'react-router-dom';
 import { fetchVideos } from '../actions';
 
 //component
-//import SwitchVid from '../components/SwitchVid';
 import Video from '../components/video';
 import Thumb from '../components/Thumb';
 
