34d2c9ac4f3558351ee2cbdc0daf5c2105ee07cc
vince4128 <vrlt.vincent@gmail.com>
2018-09-06 08:02:08 +0200

    new splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
+import { fetchCategories } from '../actions';
 
 const Splash = () => {
     return(
         <Link to="/films">
         <section className="o-splash">
-            <article id="a-splash-enter">LARS BLUMERS</article>
+            <article id="a-splash-enter">
+            <h1>LARS BLUMERS</h1>
+            <h2>ENTER</h2>
+            <ul>
+                <li>Films</li>
+                <li>Commercials</li>
+                <li>Strange things</li>
+                <li>Projects</li>                
+            </ul>
+            </article>
         </section>
         </Link>
     )
